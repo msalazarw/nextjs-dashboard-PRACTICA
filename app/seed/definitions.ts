a76dc9437125bf6117b2dc2b3c22bd5ef6669255
msalazarw <89816226+msalazarw@users.noreply.github.com>
2024-10-23 11:14:13 -0300

    commit2

---
+++ app/seed/definitions.ts
@@ -0,0 +1,7 @@
+// app/seed/definitions.ts
+export interface Revenue {
+  amount: number;
+  date: string;
+  revenue: number; // Añade esta línea si 'revenue' es una propiedad de los objetos que manejas.
+  // otros campos según tus necesidades
+}
